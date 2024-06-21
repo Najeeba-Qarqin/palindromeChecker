0446ec1d2cf8aef91a874c8c17a0405211d95309
Najeeba <najeebaqarqin@gmail.com>
2024-06-21 15:46:06 +0430

    Solved linter errors in css and js file

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const txtInput = document.querySelector('.inputs input'),
-  checkBtn = document.querySelector('.inputs button'),
-  infoTxt = document.querySelector('.info-text');
+const txtInput = document.querySelector('.inputs input');
+const checkBtn = document.querySelector('.inputs button');
+const infoTxt = document.querySelector('.info-text');
 let filterInput;
-checkBtn.addEventListener("click", () => {
-  filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
-  let reverseInput = filterInput.split("").reverse().join("");
-  let text = document.querySelector('.inputs input').value;
+checkBtn.addEventListener('click', () => {
+  filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, '');
+  const reverseInput = filterInput.split('').reverse().join('');
+  const text = document.querySelector('.inputs input').value;
   if (text === '') {
-    return alert('Please input a value');
+    return window.alert('Please input a value');
   }
-  infoTxt.style.display = "block";
-  if (filterInput != reverseInput) {
-    return infoTxt.innerHTML = `No <span>'${txtInput.value}'</span> is not a palindrome`;
+  infoTxt.style.display = 'block';
+  if (filterInput !== reverseInput) {
+    infoTxt.innerHTML = `No <span>'${txtInput.value}'</span> is not a palindrome`;
   }
-  infoTxt.innerHTML = `Yes <span>'${txtInput.value}'</span> is a palindrome`;
+  return infoTxt.innerHTML = `Yes <span>'${txtInput.value}'</span> is a palindrome`;
 });
-txtInput.addEventListener("keyup", () => {
-  filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
+txtInput.addEventListener('keyup', () => {
+  filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, '');
   if (filterInput) {
-    return checkBtn.classList.add("active");
+    return checkBtn.classList.add('active');
   }
-  infoTxt.style.display = "none";
-  checkBtn.classList.remove("active");
+  infoTxt.style.display = 'none';
+  return checkBtn.classList.remove('active');
 });
